881495a5b66d8ef2afcf75acf5c2d8cf61d0461d
Sonnche161115 <133341834+Sonnche161115@users.noreply.github.com>
2026-08-12 10:15:30 +0700

    lag

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,18 +5,18 @@ function Footer() {
     <footer className="app-footer">
       <Container className="text-center">
         <p>
-          <span className="footer-label">Student Name:</span> Nguyễn Quang Huy
+          <span className="footer-label">Student Name:</span> Nguyễn Cao Sơn
         </p>
         <p>
-          <span className="footer-label">Student ID:</span> HE186466
+          <span className="footer-label">Student ID:</span> HE161115
         </p>
         <p>
           <span className="footer-label">Class:</span> SE19D01
         </p>
         <p>
           <span className="footer-label">Email:</span>{" "}
-          <a href="huy66786@fpt.edu.vn" style={{ color: "#e74c3c" }}>
-            huy66786@fpt.edu.vn
+          <a href="sonnche161115@fpt.edu.vn" style={{ color: "#e74c3c" }}>
+            sonnche161115@fpt.edu.vn
           </a>
         </p>
       </Container>
